9f5e4257628bfb75d73d79db85921274c130e54d
Кривой Иван <IKrivoy.SBT@sberbank.ru>
2017-06-27 16:29:32 +0300

    master clean

--- index.js
+++ index.js
@@ -38,8 +38,6 @@ var Cookie = tough.Cookie;
 axiosCookieJarSupport(axios);
 const cookieJar = new tough.CookieJar();
 
-//var bodyParser = require('body-parser');
-//var jsonParser = bodyParser.json();
 
 
 var instance = axios.create({
@@ -54,93 +52,6 @@ var instance = axios.create({
   }
 });
 
-
-
-/*
-var xml = '<?xml version="1.0" encoding="UTF-8"?>' +
-  '<root>' +
-  '<child foo="bar">' +
-  '<grandchild>1 content</grandchild>' +
-  '<ff><grandchild>2 content</grandchild></ff>' +
-  '<grandchild>3 content</grandchild>' +
-  '<grandchild>4 content</grandchild>' +
-  '<grandchild>5 content</grandchild>' +
-  '</child>' +
-  '<sibling>with content!</sibling>' +
-  '</root>';
-var xmlDoc = libxml.parseXmlString(xml);
-
-// xpath queries
-var gchild = xmlDoc.get('//grandchild');
-
-while (gchild != null) {
-  console.log(gchild.text());
-  gchild = gchild.nextElement()
-
-}
-*/
-
-
-// prints "grandchild content"
-/*
-var children = xmlDoc.root().childNodes();
-
-var child = children[0];
-
-console.log(child.value)
-try {
-  console.log(child.attr('foo').value()); // prints "bar"
-} catch (e) {
-  console.log(e);
-}
-var arr = xmlDoc.find('//grandchild');
-for (var val in arr) {
-  // xmlDoc2 = libxml.parseXmlString('<?xml version="1.0" encoding="UTF-8"?>' +
-  //   '<root>' + arr[val]
-  //   '</root>');
-  var mlDoc2 = libxml.parseXmlString(arr[val]).get('//grandchild').text();;
-
-  console.log(mlDoc2); // prints "grandchild content"
-  //console.log("obj." + val + " = " + arr[val]);
-}
-console.log(Object.getOwnPropertyNames(arr).sort());
-console.log(arr[0])
-
-
-*/
-/*
-var aut1 = aut(PSI_ROZA.HOST +
-  '/CSAMAPI/registerApp.do?operation=register&login=' + PSI_ROZA.LOGIN +
-  '&version=' + GLOBALS.VERSION +
-  '.10&appType=iPhone&appVersion=5.5.0&deviceName=Simulator&devID=' +
-  GLOBALS.DEVID).then((response) => {
-
-  var encodedData = windows1251.encode(response.data);
-  var str = encodedData.replace("windows-1251", "UTF-8")
-
-
-  var xmlDoc = libxml.parseXmlString(str);
-
-  // xpath queries
-  var gchild = xmlDoc.get('//response/confirmRegistrationStage/mGUID').text();
-  // cookieJar.setCookieSync('JSESSIONID=' + gchild, PSI_ROZA.HOST_BLOCK +
-  //   '/mobile' + GLOBALS.VERSION + '/postCSALogin.do');
-  //cookieJar.setCookieSync('JSESSIONID=' + gchild, PSI_ROZA.HOST_BLOCK+'/mobile'+GLOBALS.VERSION+'/private/payments/list.do);'
-  //console.log(gchild); // prints "grandchild content"
-  //console.log(response.status);
-  //console.log(cookieJar.toJSON());
-  // var cookie = Cookie.parse("mGUID=" + gchild +
-  //   "; Domain=194.186.207.23; Path=/");
-  // var cook = cookieJar.setCookieSync(cookie, PSI_ROZA.HOST);
-  // console.log(cook);
-
-})
-
-
-.catch(function(error) {
-  console.log(error)
-});
-*/
 
 
 function httpGet(url1) {
@@ -170,8 +81,7 @@ var aut = function(addr, val) {
     var gchild = xmlDoc.get(val);
     console.log(val + ': ' + gchild.text()); // prints "grandchild content"
     return gchild.text();
-    //console.log("aut2 status: " + response.status);
-    //console.log(response.data)
+
   }).catch(function(error) {
     console.log(error)
   });
@@ -181,25 +91,15 @@ var aut2 = function(addr) {
   return instance.post(addr).then(response => {
     var iconv = new Iconv('windows-1251', 'UTF-8');
     var encodedData = iconv.convert(response.data).toString();
-    //var encodedData = windows1251.encode(response.data);
-    var str = encodedData.replace("windows-1251", "UTF-8")
-      //var xmlDoc = libxml.parseXmlString(str);
 
-    //var gchild = xmlDoc.get(val);
-    //console.log(val + ': ' + gchild.text());
+    var str = encodedData.replace("windows-1251", "UTF-8")
+
     return str;
-    //console.log("aut2 status: " + response.status);
-    //console.log(response.data)
+
   }).catch(function(error) {
     console.log(error)
   });
 };
-
-// var iconv = new Iconv('windows-1251', 'UTF-8');
-// var x = iconv.convert(
-//   '<?xml version="1.0" encoding="windows-1251" ?><response>     <status>         <code>0</code>     </status>         <loginCompleted>true</loginCompleted>     <person>         <surName>ццццц</surName>         <firstName>цццццц</firstName>         <patrName>ццццц</patrName>             <lastLogonDate>27.06.2017T14:53</lastLogonDate>                 <lastIpAddress>194.186.207.251</lastIpAddress>         <creationType>UDBO</creationType>         <region>                    <id>0</id>                     <name>ццццццц</name>         </region>                 <mbstatus>mbk</mbstatus>             <loginId>5000406014</loginId>             <TARIF_PLAN>0</TARIF_PLAN>              <localeId>RU</localeId>     </person>             <isLightSchemeERIB>false</isLightSchemeERIB>         <isAfterRegistration>true</isAfterRegistration> </response>'
-// );
-// console.log(x.toString());
 
 
 
@@ -234,99 +134,7 @@ aut(PSI_ROZA.HOST +
   }).then(ttt => {
     console.log(ttt);
   })
-  // .then(
-  //   aut(PSI_ROZA.HOST + "/CSAMAPI/registerApp.do?operation=confirm&mGUID=" +
-  //     mGUID + "&smsPassword=" + PSI_ROZA.SMS_PASS + "&version=" + GLOBALS.VERSION +
-  //     ".10&appType=iPhone");
-  //
-  //
-  // )
-
 
 .catch(function(error) {
   console.log(error)
 });
-
-
-
-/*
-httpGet(
-    PSI_ROZA.HOST +
-    '/CSAMAPI/registerApp.do?operation=register&login=' + PSI_ROZA.LOGIN +
-    '&version=' + GLOBALS.VERSION +
-    '.10&appType=iPhone&appVersion=5.5.0&deviceName=Simulator&devID=' +
-    GLOBALS.DEVID
-  )
-  // 1. Получить данные о пользователе в JSON и передать дальше
-  .then(response => {
-    var encodedData = windows1251.encode(response.data);
-    var str = encodedData.replace("windows-1251", "UTF-8")
-
-
-    var xmlDoc = libxml.parseXmlString(str);
-
-    // xpath queries
-    var gchild = xmlDoc.get('//response/confirmRegistrationStage/mGUID');
-
-    //console.log(gchild.text()); // prints "grandchild content"
-
-    //console.log(response);
-    return gchild.text();
-  })
-  // 2. Получить информацию с github
-  .then(mguid => {
-    //console.log(user);
-    return httpGet(
-      user
-    );
-  })
-  // 3. Вывести аватар на 3 секунды (можно с анимацией)
-  .then(githubUser => {
-    console.log(githubUser); // (*)
-  });
-
-*/
-
-/*
-var aut2 = aut(PSI_ROZA.HOST +
-    '/CSAMAPI/registerApp.do?operation=register&login=' + PSI_ROZA.LOGIN +
-    '&version=' + GLOBALS.VERSION +
-    '.10&appType=iPhone&appVersion=5.5.0&deviceName=Simulator&devID=' +
-    GLOBALS.DEVID).then(function(response) {
-    console.log("aut2: " + response.status);
-  })
-  .catch(function(error) {
-    return error;
-  });
-
-var aut3 = aut(PSI_ROZA.HOST +
-    '/CSAMAPI/registerApp.do?operation=register&login=' + PSI_ROZA.LOGIN +
-    '&version=' + GLOBALS.VERSION +
-    '.10&appType=iPhone&appVersion=5.5.0&deviceName=Simulator&devID=' +
-    GLOBALS.DEVID).then(function(response) {
-    console.log("aut3: " + response.status);
-  })
-  .catch(function(error) {
-    return error;
-  });
-*/
-
-// aut(PSI_ROZA.HOST +
-//     '/CSAMAPI/registerApp.do?operation=register&login=' + PSI_ROZA.LOGIN +
-//     '&version=' + GLOBALS.VERSION +
-//     '.10&appType=iPhone&appVersion=5.5.0&deviceName=Simulator&devID=' +
-//     GLOBALS.DEVID).then(function(response) {
-//     console.log(response.status);
-//   })
-//   .catch(function(error) {
-//     console.log("Error2" + error);
-//   });
-
-
-
-// Promise.resolve(aut1)
-//   .then(aut2)
-//   .then(aut3)
-//   .catch(function(error) {
-//     console.log("Error2" + error);
-//   });
